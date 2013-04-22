a23941872463502f544f805b3e778ca49c3bf10c
Nickolay Abdrafikov <nicck.olay@gmail.com>
2013-04-23 01:38:40 +0400

    add theunarchiver and anvil manifests

---
+++ lib/anvil.sh
@@ -0,0 +1,7 @@
+name      Anvil
+version   0.76
+homepage  http://anvilformac.com
+provider  zip
+url       http://sparkler.herokuapp.com/apps/3/download
+
+
---
+++ lib/theunarchiver.sh
@@ -0,0 +1,6 @@
+name      "The Unarchiver"
+version   3.7
+homepage  https://code.google.com/p/theunarchiver
+provider  zip
+url       https://theunarchiver.googlecode.com/files/TheUnarchiver3.7.zip
+

bc48e50437c7de882321ea4ef589d96e518c39cd
Nickolay Abdrafikov <nicck.olay@gmail.com>
2013-08-01 23:31:21 +0400

    add steam client

---
+++ lib/steam.sh
@@ -0,0 +1,7 @@
+name      Steam
+version   0
+homepage  http://store.steampowered.com/
+provider  dmg
+url       http://media.steampowered.com/client/installer/steam.dmg
+
+# ln -s ~/Library/Application\ Support/Steam/SteamApps /Volumes/HDD/_Games/SteamApps

3c7fabf7d8dd7e4d03dc569c88cd49297f721e01
Nickolay Abdrafikov <nicck.olay@gmail.com>
2013-08-01 23:32:00 +0400

    update sublime3-bootstrap script url

--- lib/sublime3.sh
+++ lib/sublime3.sh
@@ -5,5 +5,5 @@ provider  dmg
 url       "http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%203017.dmg"
 
 configure () {
-  curl -fsSL https://raw.github.com/nicck/sublime-bootstrap/st3/bootstrap-osx.sh | sh
+  curl -fsSL http://git.io/sublime3-bootstrap | sh
 }

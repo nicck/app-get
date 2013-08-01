b837b22ab4111c76d772f351394c0ff25a41b324
Nickolay Abdrafikov <nicck.olay@gmail.com>
2013-08-01 23:41:19 +0400

    update st3 to beta

--- lib/sublime3.sh
+++ lib/sublime3.sh
@@ -1,8 +1,8 @@
 name      "Sublime Text"
-version   203017dev
-homepage  http://www.sublimetext.com/3dev
+version   203047dev
+homepage  http://www.sublimetext.com/3
 provider  dmg
-url       "http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%203017.dmg"
+url       "http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%203047.dmg"
 
 configure () {
   curl -fsSL http://git.io/sublime3-bootstrap | sh
